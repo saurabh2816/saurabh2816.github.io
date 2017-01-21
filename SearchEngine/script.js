0ac6df1f49bcff5ab79759e754353cffd68d3297
saurabh <saurabh2816@gmail.com>
2017-01-21 15:15:54 -0500

    changes

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,8 +1,18 @@
-var request = new XMLHttpRequest();
-
-request.open('GET', 'data.txt', false);
-request.send();
-if(request.status == 200){
-console.log(request);
-document.writeln("yesssssss");
+var request;
+if(window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = ActiveXObject("Microsoft.XMLHTTP");
 }
+var	method = 'GET',
+		url = 'data.txt';
+
+request.open(method, url);
+
+function pressButton() {
+	if(request.status == 200 && request.readyState == 4){
+		var el = document.getElementbyId('update');
+		el.innerHTML = request.responseText;
+ 	}
+}
+request.send();
