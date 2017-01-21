48ab01cf5c5598425b25c2d2b4118ef60d2b3a8d
saurabh <saurabh2816@gmail.com>
2017-01-21 13:59:23 -0500

    changes

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -4,5 +4,5 @@ request.open('GET', 'data.txt', false);
 request.send();
 if(request.status == 200){
 console.log(request);
-document.writeln(request.responseText);
+document.writeln("yesssssss");
 }
