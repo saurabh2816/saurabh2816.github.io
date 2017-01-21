92e3c4dce4f039160ca717f7ad19a50a629e8ab1
saurabh <saurabh2816@gmail.com>
2017-01-21 13:57:18 -0500

    changes

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,7 +1,8 @@
-console.log("hey");
-	var request = new XMLHttpRequest();
-	request.open('GET', 'data.txt', false);
-	request.send();
-			console.log(request);
-		document.writeln(request.responseText);
-	
+var request = new XMLHttpRequest();
+
+request.open('GET', 'data.txt', false);
+request.send();
+if(request.status == 200){
+console.log(request);
+document.writeln(request.responseText);
+}
