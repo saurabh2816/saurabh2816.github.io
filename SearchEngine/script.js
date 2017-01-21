ede23fac5ef3526dd232ec9e381fcba366b567b9
saurabh <saurabh2816@gmail.com>
2017-01-21 16:09:20 -0500

    changes

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -13,8 +13,7 @@ request.onreadystatechange = function() {
 	if(request.status === 200 && request.readyState === 4){
 		var doc = request.responseXML;
 		console.log(doc);
-		console.log(doc.getElementsByTagName('item')[0].getElementsByTagName('title'));
-		//console.log(doc.getElementsByTagName('channel'));
+		console.log(doc.getElementsByTagName('item')[0].getElementsByTagName('title').innerHTML);
  	}
 }
 request.send();
