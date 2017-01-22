df4112cf7247e1968a8d00481b989457a2bf3fbd
saurabh <saurabh2816@gmail.com>
2017-01-22 00:47:23 -0500

    techcrunch headings

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,5 +1,6 @@
 //jquery takes care of backwards compatibility in browsers
 $('#search').keyup(function() {
+
   var searchField = $('#search').val();
   console.log(searchField);
   var myExp = new RegExp(searchField, "i"); // case insensitive search
@@ -8,9 +9,9 @@ $('#search').keyup(function() {
     var output = "<ul class='seachresults'>";
     var items = data.items;
     for(var i=0; i<items.length; i++){
-      if(items[i].title.search(myExp) != -1) {
+      if((items[i].title.search(myExp) != -1) || (items[i].description.search(myExp) != -1 ))  {
       output += '<br><li>';
-      output += '<a href="' + items[i].link + '">' + items[i].title + '</a>';
+      output += '<a href="' + items[i].link + '">' + '<h2>' + items[i].title + '</h2>' + '</a>';
       output += '<br><img src="' + items[i].thumbnail + ' height="100" width="100"/>'
       output +=  items[i].description.replace(/<(?:.|\n)*?>/gm, '') + '<br> <br>';
       output += '</li>';
