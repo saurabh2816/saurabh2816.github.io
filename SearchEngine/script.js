95644544fcbe345c678ad9e9cfed17cf86caa7ca
saurabh <saurabh2816@gmail.com>
2017-01-21 20:47:24 -0500

    techcrunch headings

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,7 +1,7 @@
 //jquery takes care of backwards compatibility in browsers
 $.getJSON('data.json', function(data) {
   var output = "<ul class='seachresults'>";
-  $.each(data, function(key, item) {
+  $.each(data, function(key, val) {
     output += '<li>';
     output += '<h2>' + val.name + '</h2>';
     output += '</li>';
