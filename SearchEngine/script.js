c418bf9c55425b6a664efb472690dd420f987080
saurabh <saurabh2816@gmail.com>
2017-01-21 21:26:54 -0500

    techcrunch headings

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,12 +1,20 @@
 //jquery takes care of backwards compatibility in browsers
 $.getJSON('techcrunch.json', function(data) {
-  // var output = "<ul class='seachresults'>";
+  var output = "<ul class='seachresults'>";
   // $.each(data, function(key, val) {
   //   output += '<li>';
-  //   output += '<h2>' + val.name + '</h2>';
+  //   output += '<h2>' + val.items + '</h2>';
   //   output += '</li>';
   // });
   // output += '</ul>';
-  //$('#update').html(data);
-  console.log(data.items[0].title);
+  var items = data.items;
+  for(var i=0; i<items.length; i++){
+    output += '<li>';
+    output += '<a href="' + items[i].link + '">' + items[i].title + '</a>';
+    output += '<br><img src="' + items[i].thumbnail + ' height="100" width="100"/>'
+    output +=  items[i].description.replace(/<(?:.|\n)*?>/gm, '') + '<br> <br>';
+    output += '</li>';
+  }
+  $('#update').html(output);
+  //console.log(data.items[0].title);
 });
