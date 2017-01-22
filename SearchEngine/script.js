66ee20a827648b3bbe36c436750838ff769e1cb6
saurabh <saurabh2816@gmail.com>
2017-01-21 23:51:33 -0500

    techcrunch headings

--- SearchEngine/script.js
+++ SearchEngine/script.js
@@ -1,20 +1,22 @@
 //jquery takes care of backwards compatibility in browsers
-$.getJSON('techcrunch.json', function(data) {
-  var output = "<ul class='seachresults'>";
-  // $.each(data, function(key, val) {
-  //   output += '<li>';
-  //   output += '<h2>' + val.items + '</h2>';
-  //   output += '</li>';
-  // });
-  // output += '</ul>';
-  var items = data.items;
-  for(var i=0; i<items.length; i++){
-    output += '<br><li>';
-    output += '<a href="' + items[i].link + '">' + items[i].title + '</a>';
-    output += '<br><img src="' + items[i].thumbnail + ' height="100" width="100"/>'
-    output +=  items[i].description.replace(/<(?:.|\n)*?>/gm, '') + '<br> <br>';
-    output += '</li>';
+$('#search').keyup(function() {
+  var searchField = $('#search').val();
+  console.log(searchField);
+  var myExp = new RegExp(searchField, "i"); // case insensitive search
+
+  $.getJSON('techcrunch.json', function(data) {
+    var output = "<ul class='seachresults'>";
+    var items = data.items;
+    for(var i=0; i<items.length; i++){
+      if(items[i].title.search(myExp) != -1) {
+      output += '<br><li>';
+      output += '<a href="' + items[i].link + '">' + items[i].title + '</a>';
+      output += '<br><img src="' + items[i].thumbnail + ' height="100" width="100"/>'
+      output +=  items[i].description.replace(/<(?:.|\n)*?>/gm, '') + '<br> <br>';
+      output += '</li>';
+    }
   }
-  $('#update').html(output);
-  //console.log(data.items[0].title);
+    $('#update').html(output);
+    //console.log(data.items[0].title);
+  });
 });
