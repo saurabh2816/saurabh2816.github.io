2698e3c55cc45b520a5e3648aecc33cd067a7cde
Saurabh <saurabh2816@gmail.com>
2018-10-01 22:53:22 -0400

    sync

--- react/react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
-
-import "./styles.css";
-
-var root = document.getElementById("root");
-
-ReactDOM.render(<Layout />, root);
