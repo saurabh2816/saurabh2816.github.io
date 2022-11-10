8a5d6c0c0c6f460d66951295603c98657dbeff88
saurabh2816 <saurabh2816#>
2022-11-09 20:21:20 -0800

    ye le github ek aur commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   // apiURL : "http://opensourceflix.herokuapp.com/api/v1/"
-  apiURL : "http://localhost:8080/api/v1/"
+  // apiURL : "http://localhost:8080/api/v1/"
+  apiURL : "https://openflix-app.herokuapp.com/api/v1/"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
