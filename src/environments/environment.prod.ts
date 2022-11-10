d56d4788bf0a8ca586b6c853643d5668294d36e6
saurabh2816 <saurabh2816#>
2022-11-09 21:49:36 -0800

    kuch toh kiya hai kuch ho gya hai

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiURL: "http://localhost:8080/api/v1/"
-  apiURL: "http://opensourceflix.herokuapp.com/api/v1/"
+  apiURL: "https://opensourceflix.herokuapp.com/api/v1/"
 };
