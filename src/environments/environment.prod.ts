18f9a149f6f65a0ce0f010435be39fc9f7cb0b7e
saurabh2816 <saurabh2816#>
2022-11-22 13:47:03 -0800

    changing API URL to railway from Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiURL: "https://localhost:8080/api/v1/"
-  apiURL: "https://openflix-app.herokuapp.com/api/v1/"
+  apiURL: "https://web-production-42ed.up.railway.app/api/v1/"
 };
