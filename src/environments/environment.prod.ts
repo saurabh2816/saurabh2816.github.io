44f352bf9b2281c44db5c413eb6dbe37fa46d93a
saurabh2816 <saurabh2816#>
2022-11-21 17:05:35 -0800

    committing everything

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiURL: "http://localhost:8080/api/v1/"
+  // apiURL: "https://localhost:8080/api/v1/"
   apiURL: "https://openflix-app.herokuapp.com/api/v1/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiURL : "http://opensourceflix.herokuapp.com/api/v1/"
+  // apiURL : "https://opensourceflix.herokuapp.com/api/v1/"
   apiURL : "http://localhost:8080/api/v1/"
   // apiURL : "https://openflix-app.herokuapp.com/api/v1/"
 };
